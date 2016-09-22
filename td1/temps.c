55593588c96dc8137a87ade8ce726a976cdbbdcd
Whilmaud <arnauddufour8@gmail.com>
2016-09-22 12:56:23 +0200

    on continue les calcules de temps : cas 1s et 0s done

--- td1/temps.c
+++ td1/temps.c
@@ -13,10 +13,19 @@ void division_s(int *s, int * m,int *h)
 int main()
 {
 	int s,m,h;
+	m = h = 0;
 	
-	division_s(&s,&m,&h);
+	s = 0;
 	
-	printf("")
+	if(s == 0 || s == 1) // si il y a 1 ou 0 sec on print directement sans calcule pour avoir 
+	{					 // seconde au singulier
+		printf("\n %d seconde corespond a %d heure %d minute %d seconde \n",s,h,m,s);
+	}
+	else
+	{
+		division_s(&s,&m,&h);
 	
+		printf("%d secondes ",s);
+	}
 	return 0;
 }
