b1232a1f247ea26ba13c802af192b47a93cdf998
Whilmaud <arnauddufour8@gmail.com>
2016-09-22 12:27:41 +0200

    on commence les etoiles

---
+++ td1/etoile.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+/* on va creer des etoiles*/
+
+int main()
+{
+	printf("        *");
+	printf("        *");
+	printf("        *");
+	printf("        *");
+	printf("        *");
+	printf("        *");
+	printf("        *");
+	printf("        *");
+	printf("        *");
+	
+	return 0;
+}
