eef5e4db15e2e64d6c819769b2bd5d88bbf8fd91
Whilmaud <arnauddufour8@gmail.com>
2016-09-29 11:51:11 +0200

    fin de la limte :1.6...

--- td1/Limite.c
+++ td1/Limite.c
@@ -3,8 +3,20 @@
 #include <time.h>
 
 
+
 int main()
 {
+	float S,i;
+	S = 0. ;
+	i = 1. ;
+	
+	while((S+(1/(i*i)) - S) > 0.0001)
+	{
+		S = S+(1/(i*i));
+		i = i + 1.;
+	}
+	
+	printf("%f\n",S);
 	
 	return 0;
 }
