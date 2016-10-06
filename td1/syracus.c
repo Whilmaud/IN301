66f60fa0c66cc89ef5087c35ef1a7678eaf0721d
Whilmaud <arnauddufour8@gmail.com>
2016-10-06 11:37:27 +0200

    fin de syracus.c

---
+++ td1/syracus.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int syrac_count(int u0)
+{
+	int count;
+	count = 0;
+	
+	while(u0 != 1)
+	{
+		if(u0 & 1)
+		{
+			u0 = 3*u0+1;
+		}
+		else
+		{
+			u0 = u0 >> 1;
+		}
+			
+		count++;
+	}
+	
+	//printf("%d\n",count);
+	
+	return count;
+}
+
+int main()
+{
+	int n,i,o;
+	
+	n = 30;
+	
+	for(i = 1;i<n+1;i++)
+	{
+		o = syrac_count(i);
+		printf("%d prend %d operation\n",i,o);
+	}
+	
+	return 0;
+}
