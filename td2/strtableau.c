f8afe66454443d2defb695721585828d20447ec7
Whilmaud <arnauddufour8@gmail.com>
2016-10-13 11:49:35 +0200

    initialisation produit affichage de tablea

--- td2/strtableau.c
+++ td2/strtableau.c
@@ -9,24 +9,78 @@ struct Tableau {
   int tab[100];
 };
 
-/*int alea(int n)
+int alea(int n)
 {
-	
-}*/
-
-int mr()
-{
-	return rand();
+	int r;
+	static int first = 0;
+   
+	if (first == 0)
+	{
+		srand (time (NULL));
+		first = 1;
+	}
+	r = rand ();
+	return r%(n+1);
 }
+
+void init_tableau(TABLEAU *T)
+{
+	int i;
+	(*T).taille = 10;
+	for(i = 0;i < (*T).taille;i++)
+	{
+		(*T).tab[i] = alea(20);
+	}
+}
+
+void affiche_tableau(TABLEAU *T)
+{
+	int i;
+	
+	for(i = 0;i<(*T).taille;i++)
+	{
+		printf ("%d\n", (*T).tab[i]);
+	}
+}
+
+long int produit_tableau(TABLEAU *T)
+{
+	int i,x;
+	
+	x = 1;
+	
+	for(i = 0;i<(*T).taille;i++)
+	{
+		x = x * (*T).tab[i];
+	}
+	return x;
+}
+
+/*int mr()
+{
+	static int first = 0;
+   
+   if (first == 0)
+   {
+      srand (time (NULL));
+      first = 1;
+   }
+   return (rand ());
+}*/
 
 
 int main()
 {
 	int i;
-	for(i = 0;i<100;i++)
-	{
-		printf ("%d\n", mr());
-	}
+	TABLEAU T;
+	
+	init_tableau(&T);
+	
+	printf("%d\n",T.taille);
+	
+	affiche_tableau(&T);
+	
+	printf("%ld\n",produit_tableau(&T));
 	
 	return 0;
 }
