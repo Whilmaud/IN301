e84ee467d7148f42bfcf53b4708e9405e79c7377
Whilmaud <arnauddufour8@gmail.com>
2016-10-13 11:14:59 +0200

    fin de les inversion avec pointeur

--- td2/inversetreinit.c
+++ td2/inversetreinit.c
@@ -10,15 +10,40 @@ void inverse(int *p1,int *p2)
 	*p2 = save;
 }
 
+void reinitPointeur(int *p)
+{
+	int *d;
+	d = &p;
+	/*printf("%p\n",d);
+	printf("%p\n",p);
+	printf("\n");*/
+	*d = NULL;
+	/*printf("%d\n",*d);
+	printf("%p\n",d);
+	printf("%p\n",p);
+	printf("%d\n",*p);*/
+}
+
 int main()
 {
 	int a,b;
+	int* p;
 	a = 4;
 	b = 8;
 	
 	printf("%d   %d\n",a,b);
 	inverse(&a,&b);
 	printf("%d   %d\n",a,b);
+	
+	p = &a;
+	
+	printf("%p\n",p);
+	printf("\n");
+	
+	reinitPointeur(p);
+	
+	printf("\n");
+	printf("%p\n",p);
 	
 	return 0;
 }
